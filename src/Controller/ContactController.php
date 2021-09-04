d5a9e934cf11d6551868d85a21eff619c667721a
jd3000 <jd.long@hotmail.fr>
2021-09-04 16:02:34 +0200

    htmlTemplate

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,12 +5,20 @@ namespace App\Controller;
 
 use App\Entity\Prospect;
 use App\Form\ProspectType;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Message;
+use Symfony\Component\Mime\Part\DataPart;
+use Symfony\Component\Mime\Part\TextPart;
+use Symfony\Component\Mime\Header\Headers;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mime\Part\Multipart\RelatedPart;
+use Symfony\Component\Mime\Part\Multipart\AlternativePart;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 
 
 class ContactController extends AbstractController
@@ -28,14 +36,61 @@ class ContactController extends AbstractController
 
             $prospect = $form->getData();
 
-            $email =  (new Email())
+            // # 3
+            $email = (new TemplatedEmail())
                 ->from($form->get('prospectEmail')->getData())
-                ->to('contact@tekoma.com')
+                ->to(new Address('contact@tekoma.com'))
                 ->subject('CONTACT - ' . $form->get('subject')->getData())
+
                 // path of the Twig template to render
-                ->html('<b>Sujet :</b>' . ' ' . $form->get('subject')->getData() . '<br><b>Message :</b>' . ' ' . $form->get('message')->getData() . '<br><b>Identité :</b>' . ' ' . $form->get('firstname')->getData() . ' ' . $form->get('lastname')->getData() . '<br><b>Email :</b>' . ' ' . $form->get('prospectEmail')->getData() . '');
+                ->htmlTemplate('emails/template.html.twig')
+
+                // pass variables (name => value) to the template
+                ->context([
+                    'expiration_date' => new \DateTime('+7 days'),
+                    'firstname' => $form->get('firstname')->getData(),
+                    'lastname' => $form->get('lastname')->getData(),
+                    'prospectEmail' => $form->get('prospectEmail')->getData(),
+                    'subject' => $form->get('subject')->getData(),
+                    'message' => $form->get('message')->getData(),
+                ]);
 
             $mailer->send($email);
+
+
+            // # 2
+
+            // $headers = (new Headers())
+            //     ->addMailboxListHeader('From', ['fabien@symfony.com'])
+            //     ->addMailboxListHeader('To', ['foo@example.com'])
+            //     ->addTextHeader('Subject', 'Important Notification');
+
+            // $embeddedImage = new DataPart(fopen('img/TEKOMA.png', 'r'), null, 'image/png');
+            // $imageCid = $embeddedImage->getContentId();
+
+            // $textContent = new TextPart('Lorem ipsum...');
+            // $htmlContent = new TextPart(sprintf(
+            //     '<img src="cid:%s" width="100px"/> <h1>Lorem ipsum</h1> <p>...</p>',
+            //     $imageCid
+            // ), null, 'html');;
+            // $bodyContent = new AlternativePart($textContent, $htmlContent);
+            // $body = new RelatedPart($bodyContent, $embeddedImage);
+
+            // $email = new Message($headers, $body);
+            // $mailer->send($email);
+
+
+            // # 1
+
+            // $email =  (new Email())
+            //     ->from($form->get('prospectEmail')->getData())
+            //     ->to('contact@tekoma.com')
+            //     ->subject('CONTACT - ' . $form->get('subject')->getData())
+            //     // path of the Twig template to render
+            //     ->html('<b>Sujet :</b>' . ' ' . $form->get('subject')->getData() . '<br><b>Message :</b>' . ' ' . $form->get('message')->getData() . '<br><b>Identité :</b>' . ' ' . $form->get('firstname')->getData() . ' ' . $form->get('lastname')->getData() . '<br><b>Email :</b>' . ' ' . $form->get('prospectEmail')->getData() . '');
+
+            // $mailer->send($email);
+
             $this->addFlash('success', "Votre demande a bien été envoyée.");
             return $this->redirectToRoute('contact');
             // dump($prospect);
