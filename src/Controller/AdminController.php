8a0266fb389308b88a7522a2c42b4d6cfa92ecb1
jd3000 <jd.long@hotmail.fr>
2021-09-20 18:08:36 +0200

    admin_index_show

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,19 +2,38 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
 {
     /**    
      * @Route("/admin", name="admin")     
      */
-    public function index(): Response
+    public function index(ProductRepository $productRepo): Response
     {
+        $products = $productRepo->findAll();
+        // (“dump and die”) helper function
+        // dd($products);
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'products' => $products
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une seule creation
+     * 
+     * @Route("/admin/{slug}", name="admin_show")
+     */
+    public function show($slug, ProductRepository $productRepo): Response
+    {
+        $product = $productRepo->findOneBySlug($slug);
+        // (“dump and die”) helper function
+        dump($product);
+        return $this->render('admin/show.html.twig', [
+            'product' => $product
         ]);
     }
 }
