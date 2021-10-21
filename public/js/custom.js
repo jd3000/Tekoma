25a4e2a7c82df00dea45efdf7e23a911a68238fc
jd3000 <jd.long@hotmail.fr>
2021-10-21 12:10:08 +0200

    admin_uploadPreview

--- public/js/custom.js
+++ public/js/custom.js
@@ -21,86 +21,55 @@ function burgerToggle() {
 // admin / update.html.twig creation
 $(document).ready(function () {
     let uploadPreview = document.getElementById("uploadPreview");
+    formUpdateProduct = $("#update_creation_img");
+    formNewProduct = $("#add_creation_img");
+    form = "";
+    // console.log(form);
+    if (formUpdateProduct[0]) {
+        form = formUpdateProduct[0];
+    }
+    else if (formNewProduct[0]) {
+        form = formNewProduct[0];
+    }
+    // console.log(form);
     // console.log(uploadPreview);
-    $("#update_creation_img").on('change', function () {
-        //Get count of selected files
-        let countFiles = $(this)[0].files.length;
-        // console.log(countFiles);
-        let imgPath = $(this)[0].value;
-        // console.log(imgPath);
-        let extn = imgPath.substring(imgPath.lastIndexOf('.') + 1).toLowerCase();
-        // console.log(extn);
-        let image_holder = $("#image-holder");
-        // console.log(image_holder);
-        image_holder.empty();
-        if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg") {
-            if (typeof (FileReader) != "undefined") {
-                //loop for each file selected for uploaded.
-                for (let i = 0; i < countFiles; i++) {
-                    let reader = new FileReader();
-                    reader.onload = function (e) {
-                        image_holder.className = "imgProduct p-4";
-                        $("<img />", {
-                            "src": e.target.result,
-                            "class": "card-img"
-                        }).appendTo(image_holder);
+    if (form) {
+        $(form).on('change', function () {
+            //Get count of selected files
+            let countFiles = $(this)[0].files.length;
+            // console.log(countFiles);
+            let imgPath = $(this)[0].value;
+            // console.log(imgPath);
+            let extn = imgPath.substring(imgPath.lastIndexOf('.') + 1).toLowerCase();
+            // console.log(extn);
+            let image_holder = $("#image-holder");
+            // console.log(image_holder);
+            image_holder.empty();
+            if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg") {
+                if (typeof (FileReader) != "undefined") {
+                    //loop for each file selected for uploaded.
+                    for (let i = 0; i < countFiles; i++) {
+                        let reader = new FileReader();
+                        reader.onload = function (e) {
+                            image_holder.className = "imgProduct p-4";
+                            $("<img />", {
+                                "src": e.target.result,
+                                "class": "card-img"
+                            }).appendTo(image_holder);
+                        }
+                        image_holder.show();
+                        reader.readAsDataURL($(this)[0].files[i]);
                     }
-                    image_holder.show();
-                    reader.readAsDataURL($(this)[0].files[i]);
+                    uploadPreview.className = "col-12 col-md-4 text-center imgProduct p-4 mt-4";
+                } else {
+                    alert("Ce navigateur ne supporte pas la lecture de fichiers.");
                 }
-                uploadPreview.className = "col-12 col-md-4 text-center imgProduct p-4 mt-4";
             } else {
-                alert("Ce navigateur ne supporte pas la lecture de fichiers.");
+                alert("Merci de sélectionner une image.");
             }
-        } else {
-            alert("Merci de sélectionner une image.");
-        }
-    });
+        });
+    }
 });
-
-
-// admin / new.html.twig creation
-$(document).ready(function () {
-    let uploadPreview = document.getElementById("uploadPreview");
-    // console.log(uploadPreview);
-    $("#add_creation_img").on('change', function () {
-        //Get count of selected files
-        let countFiles = $(this)[0].files.length;
-        // console.log(countFiles);
-        let imgPath = $(this)[0].value;
-        // console.log(imgPath);
-        let extn = imgPath.substring(imgPath.lastIndexOf('.') + 1).toLowerCase();
-        // console.log(extn);
-        let image_holder = $("#image-holder-new");
-        // console.log(image_holder);
-        image_holder.empty();
-        if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg") {
-            if (typeof (FileReader) != "undefined") {
-                //loop for each file selected for uploaded.
-                for (let i = 0; i < countFiles; i++) {
-                    let reader = new FileReader();
-                    reader.onload = function (e) {
-                        image_holder.className = "imgProduct p-4";
-                        $("<img />", {
-                            "src": e.target.result,
-                            "class": "card-img"
-                        }).appendTo(image_holder);
-                    }
-                    image_holder.show();
-                    reader.readAsDataURL($(this)[0].files[i]);
-                }
-                uploadPreview.className = "col-12 col-md-4 text-center imgProduct p-4";
-            } else {
-                alert("Ce navigateur ne supporte pas la lecture de fichiers.");
-            }
-        } else {
-            alert("Merci de sélectionner une image.");
-        }
-    });
-});
-
-
-
 
 // Permet de scroller jusqu'au produit modifié
 
