3e8932fafae4d2494abb2305a5c20b8fd5da75b7
jd3000 <73708030+jd3000@users.noreply.github.com>
2022-02-02 16:13:01 +0100

    prepa_7

---
+++ translations/messages.fr.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'Vérification du formulaire' => "Vérification du formulaire",
+    'Message' => "Message",
+    'agree terms' => "J'accepte l'utilisation de ces informations pour être recontacté par Tekoma",
+    'your message ordering ' => "Votre message pour précommander la création ",
+    'Objet' => "Objet",
+    'Email' => "Email",
+    'Nom' => "Nom",
+    'Prénom' => "Prénom",
+    'product' => [
+        // id is user.login
+        'name' => 'name',
+    ],
+];
